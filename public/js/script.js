524f46ba89e38a0bb08b72ce72854be60f607dfe
Enzo Dussin <enzodussinn@gmail.com>
2021-11-10 19:18:25 -0300

    Init commit

---
+++ public/js/script.js
@@ -0,0 +1,23 @@
+const form = document.querySelector('form')
+const search = document.querySelector('input')
+const author = document.querySelector('#author')
+const releaseYear = document.querySelector('#releaseYear')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    author.textContent = 'Loading...'
+    releaseYear.textContent = ''
+
+    fetch('http://localhost:3000/api/albuns').then((res) => {
+        res.json().then((data) => {
+            if(!data.albuns[search.value]){
+                author.textContent = 'Album not found'
+                releaseYear.textContent = ''
+            } else{
+                author.textContent = data.albuns[search.value].author
+                releaseYear.textContent = data.albuns[search.value].releaseYear
+            }
+        })
+    })
+})
---
+++ src/app.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const PORT = 3000
+
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+const jsonPath = path.join(__dirname, '../public/json')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+app.use(express.static(publicDirectoryPath))
+hbs.registerPartials(partialsPath)
+
+app.get('', (req, res) => {
+    res.render('index')
+})
+
+app.get('/api/albuns', (req, res) => {
+    res.header('Content-Type', 'application/json')
+    res.sendFile(`${jsonPath}/albuns.json`)
+})
+
+app.get('*', (req, res) => {
+    res.render('404', { err: 'Code 404: page not found' })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running in PORT ${PORT}`)
+})
